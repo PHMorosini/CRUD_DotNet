6ad83608ca3ac096e5621aae4433f009f51c79a0
Pedro Morosini <phmorosini@hotmail.com>
2024-10-23 14:29:36 -0300

    Lista de clientes no evento feita

--- CRUD_DotNet/Controllers/EventosController.cs
+++ CRUD_DotNet/Controllers/EventosController.cs
@@ -4,6 +4,7 @@ using CRUD_DotNet.Services.Interfaces;
 using CRUD_DotNet.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System.Globalization;
+using Microsoft.EntityFrameworkCore;
 
 namespace CRUD_DotNet.Controllers
 {
@@ -57,10 +58,22 @@ namespace CRUD_DotNet.Controllers
         [HttpGet]
         public IActionResult Update(int id)
         {
+            var estadosViewBag = ViewBag.Estados as IEnumerable<CRUD_DotNet.Models.Estado>;
+
+            var evento = _context.Eventos.Include(e => e.Clientes)
+                .FirstOrDefault(e => e.Id == id);
+
+
+            var clientesDisponiveis = _context.Clientes
+                .Where(c => !evento.Clientes.Contains(c))
+                .ToList();
+
+            ViewBag.ClientesDisponiveis = clientesDisponiveis;
             var estados = _estadoServices.GetEstados();
             ViewBag.Estados = estados;
 
-            var evento = _context.Eventos.Find(id);
+            ViewBag.EventoId = id;
+            
             if (evento == null)
             {
                 return NotFound();
@@ -71,7 +84,15 @@ namespace CRUD_DotNet.Controllers
         [HttpPost]
         public IActionResult Update(int id,Evento evento)
         {
-            
+            var clientesDisponiveis = _context.Clientes
+                .Where(c => !evento.Clientes.Contains(c))
+                .ToList();
+
+            ViewBag.ClientesDisponiveis = clientesDisponiveis;
+            var estados = _estadoServices.GetEstados();
+            ViewBag.Estados = estados;
+            ViewBag.EventoId = id;
+
             if (ModelState.IsValid)
             {
                
@@ -81,6 +102,35 @@ namespace CRUD_DotNet.Controllers
             }
             return View(evento);
         }
+
+        [HttpPost]
+        public JsonResult AdicionarClienteEvento(int eventoId, int clienteId)
+        {
+            var evento = _context.Eventos.Include(c => c.Clientes).FirstOrDefault(e => e.Id == eventoId);
+            var cliente = _context.Clientes.Find(clienteId);
+
+            if (cliente == null)
+            {
+                return Json(new { success = false, mensagem = "Cliente não encontrado." });
+            }
+
+            if (!evento.Clientes.Any(c => c.Id == cliente.Id))
+            {
+                evento.Clientes.Add(cliente);
+                _context.Update(evento);
+                _context.SaveChanges();
+
+                return Json(new { success = true, mensagem = "Cliente adicionado ao evento com sucesso." });
+                
+            }
+            else
+            {
+                return Json(new { success = false, mensagem = "Cliente já está adicionado a este evento." });
+            }
+        }
+
+
+
 
         [HttpPost]
         public IActionResult UpdateAtivo(int id, [FromBody] AtivoViewModel model)
--- CRUD_DotNet/wwwroot/js/Eventos.js
+++ CRUD_DotNet/wwwroot/js/Eventos.js
@@ -1,35 +1,63 @@
 ﻿$(document).ready(function() {
-    $('#valorIngresso').on('input', function () {
-        let valor = $(this).val();
-        // Remove caracteres que não sejam números ou vírgulas
-        valor = valor.replace(/[^0-9,]/g, '');
+    $('#valorIngresso').on('input',
+        function() {
+            let valor = $(this).val();
+            // Remove caracteres que não sejam números ou vírgulas
+            valor = valor.replace(/[^0-9,]/g, '');
 
-        // Verifica se há mais de uma vírgula
-        if ((valor.match(/,/g) || []).length > 1) {
-            valor = valor.slice(0, -1);
-            alert("Não é permitido inserir mais de uma vírgula.");
-        }
+            // Verifica se há mais de uma vírgula
+            if ((valor.match(/,/g) || []).length > 1) {
+                valor = valor.slice(0, -1);
+                alert("Não é permitido inserir mais de uma vírgula.");
+            }
 
-        // Verifica se o valor tem mais de 8 números antes da vírgula
-        const partes = valor.split(',');
-        if (partes[0].length > 8) {
-            partes[0] = partes[0].slice(0, 8); 
-        }
-        if (partes.length > 1 && partes[1].length > 2) {
-            partes[1] = partes[1].slice(0, 2);
-        }
-        valor = partes.join(',');
-        $(this).val(valor);
+            // Verifica se o valor tem mais de 8 números antes da vírgula
+            const partes = valor.split(',');
+            if (partes[0].length > 8) {
+                partes[0] = partes[0].slice(0, 8);
+            }
+            if (partes.length > 1 && partes[1].length > 2) {
+                partes[1] = partes[1].slice(0, 2);
+            }
+            valor = partes.join(',');
+            $(this).val(valor);
+        });
+
+    $('#btnAdicionarCliente').click(function () {
+        var idCliente = $('#clienteId').val();
+        var idEvento = $('#eventoId').val();
+
+        console.log("Cliente ID: ", idCliente);
+        console.log("Evento ID: ", idEvento);
+        console.log('@Url.Action("AdicionarClienteEvento", "Eventos")');
+        $.ajax({
+            url: '/Eventos/AdicionarClienteEvento',
+            type: 'POST',
+            data: {
+                eventoId: idEvento,
+                clienteId: idCliente
+            },
+            success: function (response) {
+                if (response.success) {
+                    alert(response.mensagem);
+                }
+            },
+            error: function () {
+                alert("Falha na comunicação com o servidor.");
+            }
+        });
     });
 });
+
 function atualizarAtivo(eventoId, isAtivo) {
-    fetch(`/Eventos/UpdateAtivo/${eventoId}`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ ativo: isAtivo })
-    })
+    fetch(`/Eventos/UpdateAtivo/${eventoId}`,
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ ativo: isAtivo })
+            })
         .then(response => {
             if (!response.ok) {
                 alert('Erro ao atualizar o estado.');
@@ -38,5 +66,6 @@ function atualizarAtivo(eventoId, isAtivo) {
         .catch(error => {
             console.error('Erro:', error);
         });
+
 }
 
